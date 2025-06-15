9474146cc5006d46d464cc64d7ce424c3c18ea5d
nickolasbradham <nickolasbradham@gmail.com>
2025-06-14 21:34:18 -0700

    V1 complete.

--- src/nbradham/ragequiter/Quiter.java
+++ src/nbradham/ragequiter/Quiter.java
@@ -12,40 +12,51 @@ import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
 
 final class Quiter {
 
-	public static final void main(String[] args) throws NativeHookException, AWTException {
+	public static final void main(final String[] args) throws NativeHookException, AWTException {
 		GlobalScreen.registerNativeHook();
-		Robot r = new Robot();
+		final Robot r = new Robot();
 		GlobalScreen.addNativeKeyListener(new NativeKeyListener() {
 
 			private static final String[] CMD = new String[] { "cmd", "/C", "ipconfig", "/release&ipconfig", "/renew" };
+			private static final byte WAIT = 10;
 
 			@Override
-			public final void nativeKeyPressed(NativeKeyEvent e) {
+			public final void nativeKeyPressed(final NativeKeyEvent e) {
 				switch (e.getKeyCode()) {
 				case NativeKeyEvent.VC_BACKQUOTE:
 					try {
 						GlobalScreen.unregisterNativeHook();
-					} catch (NativeHookException e1) {
+					} catch (final NativeHookException e1) {
 						e1.printStackTrace();
 					}
 					break;
-				case NativeKeyEvent.VC_1:
-					tap(KeyEvent.VK_T);
-					r.keyPress(KeyEvent.VK_CONTROL);
-					tap(KeyEvent.VK_V);
-					r.keyRelease(KeyEvent.VK_CONTROL);
-					tap(KeyEvent.VK_ENTER);
+				case NativeKeyEvent.VC_F1:
+					try {
+						tap(KeyEvent.VK_T);
+						press(KeyEvent.VK_CONTROL);
+						tap(KeyEvent.VK_V);
+						r.keyRelease(KeyEvent.VK_CONTROL);
+						tap(KeyEvent.VK_ENTER);
+						Thread.sleep(50);
+					} catch (final InterruptedException e1) {
+						e1.printStackTrace();
+					}
 					try {
 						Runtime.getRuntime().exec(CMD);
-					} catch (IOException e1) {
+					} catch (final IOException e1) {
 						e1.printStackTrace();
 					}
 				}
 			}
 
-			private final void tap(int key) {
-				r.keyPress(key);
+			private final void tap(final int key) throws InterruptedException {
+				press(key);
 				r.keyRelease(key);
+			}
+			
+			private final void press(final int key) throws InterruptedException {
+				r.keyPress(key);
+				Thread.sleep(WAIT);
 			}
 		});
 	}
